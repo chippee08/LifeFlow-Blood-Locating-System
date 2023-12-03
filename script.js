08b1d6e8e36dca7e12b9b5a5bec739e35e962520
chippee08 <93817250+chippee08@users.noreply.github.com>
2023-12-03 13:05:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+/* console.log("Hello World"); 
+
+var myName = "Paolo";
+
+console.log(myName);
+
+let ourName = "RTU";
+console.log(ourName);
+
+
+
+var a = 5;
+var b = 10;
+var c = "I am the ";
+
+a = a + 64;
+b = b + 59;
+c = c + " storm that is approaching!";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+var dec = 2.5 * 3;
+console.log(dec);
+*/
+
+//Escaping literal quotes
+
+//var str = "I am a \" double quoted \" string inside \" double quotes\"";
+//console.log(str);
+
+/*var ans = 1 + "1";
+console.log(ans); */
+
+
+
+function McMc(){
+   let iframe = document.getElementById("map1");
+   if (iframe.style.display === "none"){
+    iframe.style.display = "block";
+   }
+   else{
+    iframe.style.display = "none";
+   }
+}
+
+function HcMc(){
+    let iframe = document.getElementById("map2");
+   if (iframe.style.display === "none"){
+    iframe.style.display = "block";
+   }
+   else{
+    iframe.style.display = "none";
+   }
+}
+
+function vrp(){
+    let iframe = document.getElementById("map3");
+    if (iframe.style.display === "none"){
+        iframe.style.display = "block";
+    }
+    else{
+        iframe.style.display = "none";
+    }
+}
+
+
+/*function McMc(){
+    document.getElementById("map1").style.display = "block";
+    
+ } */
